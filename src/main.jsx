5263f1880d92c3b1e58c8d7e40465a04727076d1
Zadnap <88957683+zadnap@users.noreply.github.com>
2026-01-21 14:22:08 +0700

    feat(styles): define scss structure and variables

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.scss';
+import App from '@/app/App.jsx';
+import '@/styles/global.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
