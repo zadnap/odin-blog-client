04e89e4788a08421b849347660642d9da22767e5
Zadnap <88957683+zadnap@users.noreply.github.com>
2026-01-23 10:01:05 +0700

    feat(ui): add the Header component
    Besides building the Header component itself, I change the variables,
    reset css and typography to better suit the component css.

--- src/components/ui/Header/Header.jsx
+++ src/components/ui/Header/Header.jsx
@@ -1,7 +1,54 @@
+import { Link } from 'react-router';
+import logo from '@/assets/images/logo.png';
 import styles from './Header.module.scss';
 
 const Header = () => {
-  return <header className={styles.header}>Header</header>;
+  return (
+    <header className={styles.header}>
+      <section className={styles.siteBranding}>
+        <Link
+          to="/"
+          className={styles.logo}
+          aria-label="Hidden Progress homepage"
+        >
+          <img
+            className={styles.logoImage}
+            src={logo}
+            alt="Hidden Progress logo"
+          />
+          <span className={styles.logoText}>Hidden Progress</span>
+        </Link>
+        <p className={styles.motto}>Dream big, start small.</p>
+      </section>
+      <section className={styles.mainNav}>
+        <nav className={styles.navigation} aria-label="Primary navigation">
+          <ul className={styles.menu}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/posts/about-me">About Me</Link>
+            </li>
+            <li>
+              <Link to="/posts/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <ul className={styles.actions}>
+          <li>
+            <Link to="/auth/login" className={styles.loginBtn}>
+              Sign In
+            </Link>
+          </li>
+          <li>
+            <Link to="/auth/register" className={styles.registerBtn}>
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+      </section>
+    </header>
+  );
 };
 
 export default Header;
