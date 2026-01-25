e67cf6a028aedf1b43fac350f750901502f16a3c
Zadnap <88957683+zadnap@users.noreply.github.com>
2026-01-25 16:11:20 +0700

    feat(ui): add SubscribeSection component

--- src/features/home/components/SubscribeSection/SubscribeSection.jsx
+++ src/features/home/components/SubscribeSection/SubscribeSection.jsx
@@ -1,8 +1,25 @@
+import { Input, Button } from '@/components/ui';
 import styles from './SubscribeSection.module.scss';
 
 const SubscribeSection = () => {
   return (
-    <section className={styles.subscribeSection}>Subscribe Section</section>
+    <section className={styles.subscribeSection}>
+      <h2>Stay with Hidden Progress</h2>
+      <p>
+        I share lessons from learning, building, and life. No spam — just quiet
+        progress.
+      </p>
+      <form className={styles.form} noValidate>
+        <Input
+          id="email"
+          type="email"
+          label="Email address"
+          placeholder="Your email address"
+          srOnly
+        />
+        <Button>Subscribe</Button>
+      </form>
+    </section>
   );
 };
 
