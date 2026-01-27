1dc30760d593eb0f95e8ec7f059661e59953c0a2
Zadnap <88957683+zadnap@users.noreply.github.com>
2026-01-27 10:13:29 +0700

    feat(ui): add AuthForm component structure

--- src/features/auth/components/AuthForm/AuthForm.jsx
+++ src/features/auth/components/AuthForm/AuthForm.jsx
@@ -1,9 +1,65 @@
+import { Link } from 'react-router';
 import styles from './AuthForm.module.scss';
+import { Input, Button } from '@/components/ui';
 
-const AuthForm = ({ mode }) => {
+const AuthForm = ({ mode, onSubmit }) => {
+  const isRegister = mode === 'register';
+
   return (
-    <form className={styles.authForm} action="">
-      {mode}
+    <form className={styles.authForm} onSubmit={onSubmit} noValidate>
+      <header>
+        <h1>
+          {isRegister ? 'Create an account' : 'Welcome back to Hidden Progress'}
+        </h1>
+        <p>
+          {isRegister
+            ? 'Begin your journey of steady, meaningful progress.'
+            : 'Continue building with clarity and focus.'}
+        </p>
+      </header>
+      <Input
+        className={styles.input}
+        label="Username"
+        name="username"
+        placeholder="Username"
+        id="username"
+        required
+      />
+      <Input
+        className={styles.input}
+        type="password"
+        label="Password"
+        name="password"
+        placeholder="Password"
+        id="password"
+        required
+      />
+      {isRegister && (
+        <Input
+          className={styles.input}
+          type="password"
+          label="Confirm password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          id="confirmPassword"
+          required
+        />
+      )}
+      {isRegister ? (
+        <p>
+          Already have an account? <Link to="/auth/login">Login</Link>
+        </p>
+      ) : (
+        <p>
+          No account yet? <Link to="/auth/register">Register</Link>
+        </p>
+      )}
+      <Button className={styles.submitBtn} type="submit">
+        {isRegister ? 'Sign up' : 'Log in'}
+      </Button>
+      <p className={styles.goHome}>
+        Or continue without signing in <Link to="/">Go home</Link>
+      </p>
     </form>
   );
 };
